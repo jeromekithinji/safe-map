c331ff69fc827fbc292e7216ddd6fd69f4514429
Jerome Kithinji <kithinjijerome@gmail.com>
2025-04-27 09:27:40 -0400

    Improved UI

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,10 +5,12 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navbar__content">
-        <p className="navbar__content-title">SafeMap</p>
-        <p className="navbar__content-subtitle">
-          Explore crime hotspots and safety insights across the city.
-        </p>
+        <div className="navbar__content-texts">
+          <p className="navbar__content-title">SafeMap</p>
+          <p className="navbar__content-subtitle">
+            Explore crime hotspots and safety insights across the city.
+          </p>
+        </div>
       </div>
     </div>
   );
